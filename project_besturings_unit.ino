4a43efe673b6a2e309e93507a588ffbd5ebbd9ca
Tristan van der Linden <tristan@pindrop.nl>
2019-01-15 12:50:35 +0100

    First commit

---
+++ project_besturings_unit.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       project_besturings_unit.ino
+    Created:	15-1-2019 12:45:40
+    Author:     DESKTOP-985VDFK\Tristan
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
